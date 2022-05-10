3fcbeecd177efd435d5c0dc5369b13b756bcfe67
Windows 10 <triadip2@gmail,com>
2022-05-10 21:17:12 +0700

    perulangan dengan for dan while untuk jumlah buku tertentu, contoh: 10

---
+++ dsar_sintaksis_perulangan_baca_buku_denganfor.py
@@ -0,0 +1,14 @@
+"""
+Program perulangan membaca buku
+"""
+Jumlah_buku = 100
+print('Ibu berkata, "Baca semua bukumu"')
+
+Jumlah_buku_yang_sudah_dibaca = 0
+
+print(f'Jumlah buku yang sudah di baca {Jumlah_buku_yang_sudah_dibaca}')
+
+for Jumlah_buku_yang_sudah_dibaca in range(1, Jumlah_buku+1):
+    print(f"buku ke {Jumlah_buku_yang_sudah_dibaca} sduah di baca")
+
+print(f'Jumlah buku yang sudah di baca {Jumlah_buku_yang_sudah_dibaca}')
